f4c5cf181d490bff0a91e29515feaf4f04779226
薛博仁 <xueboren@xbr-pc.local>
2017-02-25 11:07:02 +0800

    初始化js文件并测试脚本成功: yarn run dev

---
+++ src/index.js
@@ -0,0 +1,15 @@
+
+class QuickStartTest {
+
+  constructor () {
+    console.log('实例初始化...')
+  }
+  static getVersion () {
+    return '1.0.0'
+  }
+
+}
+
+QuickStartTest.name = 'QuickStartTest'
+
+module.exports = QuickStartTest
