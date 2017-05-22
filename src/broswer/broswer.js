b2644b44def16c58d502f11908abe2b90e5d31c3
borenXue <xueboren001@outlook.com>
2017-05-22 16:54:37 +0800

    🚴  add function: copyText

--- src/broswer/broswer.js
+++ src/broswer/broswer.js
@@ -12,7 +12,42 @@ const isQQ = (userAgent) => {
   return /qq/i.test(userAgent || window.navigator.userAgent)
 }
 
+const copyText = (element, okCallback, errorCallback) => {
+  // ie使用clipboardData实现
+  if (window.clipboardData) {
+    window.clipboardData.clearData()
+    window.clipboardData.setData('Text', element.innerHTML)
+    if (window.clipboardData.setData('Text') === element.innerHTML) {
+      if (okCallback) okCallback()
+      return
+    }
+  }
+  // 使用createTextRange实现
+  if (element.select && element.createTextRange) {
+    element.select()
+    var therange = void 0
+    if (element.createTextRange) therange = element.createTextRange()
+    therange = therange || document
+    if (therange.execCommand && therange.execCommand('Copy')) {
+      if (okCallback) okCallback()
+      return
+    }
+  }
+  // 使用document.createRange实现
+  var range = document.createRange()
+  range.selectNode(element)
+  window.getSelection().addRange(range)
+  if (document.execCommand && document.execCommand('copy')) {
+    window.getSelection().removeAllRanges()
+    if (okCallback) okCallback()
+    return
+  }
+
+  if (errorCallback) errorCallback()
+}
+
 module.exports = {
   isWeChat,
-  isQQ
+  isQQ,
+  copyText
 }
