249c363703290970a344da591a6702ac2816020e
borenXue <xueboren001@outlook.com>
2017-06-05 20:23:02 +0800

    💧 Add eslint rules for import-from.

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,12 +12,22 @@ module.exports = {
   },
   extends: 'standard',
   plugins: [],
+  settings: {
+    "import/external-module-folders": ["src", "node_modules"],
+    "external-module-folders": ["./src", "node_modules"]
+  },
   // 少加自己的规则
   'rules': {
     'arrow-parens': 0,
     'generator-star-spacing': 0,
     'semi': 1,
     'no-trailing-spaces': 1,
+
+    'import/no-unresolved': [2, {commonjs: true, amd: true}],
+    'import/named': 2,
+    'import/namespace': 2,
+    'import/default': 2,
+    'import/export': 2,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
