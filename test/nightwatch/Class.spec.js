4dca2a0409b758eb51ddf9e31ebf61d8f401f2e3
borenXue <xueboren001@outlook.com>
2017-06-06 22:44:00 +0800

    ☁️ Add e2e test tools nightwatch、Add test spec file for e2e.

---
+++ test/nightwatch/Class.spec.js
@@ -0,0 +1,38 @@
+module.exports = {
+  'Class.js': function (browser) {
+    browser
+    .url('file:///Users/xueboren/XbrOpenSources/tools_js/dist/auto_test.html')
+    .waitForElementVisible('body', 10000, false, function() {
+      browser.assert.elementPresent("div#block-class-test.block-class-test");
+      // browser.elementIdElement('#block-class-test', 'block-class-test', 'body', function (ele) {
+      //   console.log(ele);
+      // });
+      // browser.expect.element('#main').to.be.present;
+      browser.expect.element('#block-class-test').to.have.attribute('class').contain('block-class-test');
+      let hasClass = null;
+      browser.execute(function(data) {
+        window.classs.addClass(document.querySelector("#block-class-test"), 'abcdefg');
+        hasClass = window.classs.hasClass(document.querySelector("#block-class-test"), 'abcdefg');
+        return true;
+      }, [], function(result) {
+        browser.expect.element('#block-class-test').to.have.attribute('class', 'Expected element <#block-class-test> to have attribute "class" contain: "abcdefg" after executed addClass(ele, "abcdefg") mothod.')
+          .contain('abcdefg');
+        browser.expect.element('#block-class-test').to.have.attribute('class', 'Expected element <#block-class-test> to have attribute "class" contain: "abcdefg" for executed hasClass(ele, "abcdefg") mothod.')
+          .contain('abcdefg').to.equal(hasClass);
+
+        browser.execute(function(data) {
+          window.classs.removeClass(document.querySelector("#block-class-test"), 'abcdefg');
+          hasClass = window.classs.hasClass(document.querySelector("#block-class-test"), 'abcdefg');
+          return true;
+        }, [], function(result) {
+          browser.expect.element('#block-class-test').to.have.attribute('class', 'Expected element <#block-class-test> to have attribute "class" not contain: "abcdefg" after executed removeClass(ele, "abcdefg") mothod.')
+            .not.contain('abcdefg');
+          browser.expect.element('#block-class-test').to.have.attribute('class', 'Expected element <#block-class-test> to have attribute "class" not contain: "abcdefg" for executed hasClass(ele, "abcdefg") mothod.')
+            .not.contain('abcdefg').to.equal(hasClass);
+        });
+
+      });
+      browser.end();
+    }, 'elemento %s no era visible en %d ms');
+  }
+};
---
+++ test/nightwatch/createDome.spec.js
@@ -0,0 +1,30 @@
+module.exports = {
+  'createDom': function (browser) {
+    browser
+    .url('file:///Users/xueboren/XbrOpenSources/tools_js/dist/auto_test.html')
+    .waitForElementVisible('body', 10000, false, function() {
+      this
+        .useXpath()
+        .assert.cssClassPresent("/html/body/div[@id='app']", "app")
+        .assert.cssClassPresent("/html/body/div[@id='createDom-test-one']", "test-one")
+        // attrs
+        .assert.attributeEquals("/html/body/div[@id='createDom-test-one']", "attr1", "attr-value1")
+        .assert.attributeEquals("/html/body/div[@id='createDom-test-one']", "attru", "attr-value2")
+        .assert.attributeEquals("/html/body/div[@id='createDom-test-one']", "attr3", '')
+        // datas
+        .assert.attributeEquals("/html/body/div[@id='createDom-test-one']", "data-aa", "aa")
+        .assert.attributeEquals("/html/body/div[@id='createDom-test-one']", "data-bb-bbb-def", "bb")
+        .assert.attributeEquals("/html/body/div[@id='createDom-test-one']", "data-cc-cc", "cc")
+        // content
+        .assert.containsText("/html/body/div[@id='createDom-test-one']", "哈哈哈哈哈")
+        // childs
+        .assert.containsText("/html/body/div[@id='createDom-test-one']/div[@class='child-one-div']", "child-one-div")
+        .assert.elementPresent("/html/body/div[@id='createDom-test-one']/div[@class='child-two-div']")
+        .assert.containsText("/html/body/div[@id='createDom-test-one']/div[@class='child-two-div']/span[@class='child-of-child2-content']", "child-of-child2-content")
+        .useCss()
+        .assert.containsText("body>div#createDom-test-one>.child-two-div>.child-of-child2-content", "child-of-child2-content")
+
+        .end();
+    }, 'elemento %s no era visible en %d ms');
+  }
+};
